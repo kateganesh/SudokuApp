716880405034c22ff285e394354af2967550591d
kateganesh <kateganesh@csu.fullerton.edu>
2018-07-08 19:00:18 -0700

    Moving the code from SudokuPuzzleGame Repository to current Repository

---
+++ src/main/java/com/ganesh/SudokuApp/App.java
@@ -0,0 +1,307 @@
+package com.ganesh.SudokuApp;
+
+import java.util.Scanner;
+import com.ganesh.SudokuApp.sudoku.Sudoku;
+
+public class App {
+
+	//Scanner class used to get the input from the user
+	static Scanner reader = new Scanner(System.in);
+	
+	
+	/*
+	 * Starting point of the application
+	 */
+	public static void main(String[] args) {
+	
+		//Instantiating a Sudoku class object
+		Sudoku sudoku = new Sudoku();
+		int outerChoice;
+		
+		//Loop to display the menu and accept input from user
+		do {
+			displayOuterMenu();
+			outerChoice = acceptUserInput();
+			
+			switch(outerChoice) {
+			case 1:
+				System.out.println("Board Initialized");
+				sudoku.initializeBoard();
+				sudoku.displayInput();
+				innerMenu(sudoku);
+				break;
+				
+			case 2:
+				break;
+				
+			default:
+				System.out.println("Enter correct number input");
+				
+			}
+		}while(outerChoice!=2);
+		System.out.println("Thank you.");
+		System.out.println("Application Closed");
+
+	}
+	
+	/*
+	 * Accepts the user input and maps to the relevant method
+	 */
+	private static void innerMenu(Sudoku obj) {
+		int innerChoice;
+		do {
+			displayInnerMenu();
+			innerChoice = acceptUserInput();
+			switch(innerChoice) {
+			case 1 : 
+				enterOrEditInputValue(obj);
+				break;
+			
+			case 2 :
+				removeInputValue(obj);
+				break;
+				
+			case 3:
+				checkBoard(obj);
+				break;
+				
+			case 4:
+				boolean success= submitBoard(obj);
+				if(success) {
+					innerChoice=7;
+				}
+				break;
+				
+			case 5:
+				giveupAndDisplaySolution(obj);
+				innerChoice=7;
+				break;
+				
+			case 6:
+				resetBoard(obj);
+				break;
+				
+			default:
+				System.out.println("Enter correct number input");
+				}
+		}while(innerChoice!=7);
+	}
+	
+	/*
+	 * To display the Outer Menu of the application
+	 */
+	private static void displayOuterMenu() {
+		System.out.println("********** Menu *******");
+		System.out.println("1 : Setup Board");
+		System.out.println("2 : Exit");
+		System.out.println("Enter your choice number (Between 1 and 2)");
+	}
+
+	/*
+	 * To display the Inner Menu of the Application
+	 */
+	private static void displayInnerMenu() {
+		System.out.println("******* Menu ********");
+		System.out.println("1 : Enter new value OR edit a value in Sudoku");
+		System.out.println("2 : Remove the input value");
+		System.out.println("3 : Check Board if solution is complete");
+		System.out.println("4 : Submit the solution");
+		System.out.println("5 : Give up and display Correct solution");
+		System.out.println("6 : Restart the game");
+		System.out.println("7 : Exit");
+		System.out.println("Enter your choice number (Between 1 and 2)");
+	}
+	
+	/*
+	 * Accepts the input value from the user and check and validates the entered input
+	 * to integer value only 
+	 * Returns the entered input
+	 */
+	private static int acceptUserInput() {
+		while (!reader.hasNextInt()) {
+			reader.next();
+			System.out.println("Incorrect Input! Enter integer number only");
+		}
+		int input = reader.nextInt();
+		return input;
+	}
+	
+	/*
+	 * Method gets the row number, column column and value from the user.
+	 * Checks if user is allowed to edit or input new value at that location.
+	 * Check if the entered value is correct according to sudoku logic.
+	 * Checks if the value is already present at that location, if present 
+	 * modifies the value or enter the new value
+	 */
+	private static void enterOrEditInputValue(Sudoku obj) {
+		
+		int row = acceptRowNumber();
+		int column = acceptColumnNumber();
+		int value = acceptSudokuInputValue(); 
+		
+		if(obj.canEditInputValue(row,column)==false) {
+			System.out.println("Cannot edit the number that this location");
+			return;
+		}
+		
+		if(obj.verifyInput(row, column, value)) {
+			
+			if(obj.canChangeInputValue(row,column)) {
+				obj.removeInputValue(row,column);
+				obj.enterInput(row, column, value);
+				obj.displayInput();
+			}
+			else {	
+				obj.enterInput(row, column, value);
+				obj.displayInput();
+			}	
+		}
+		else {
+			System.out.println("Not correct value at that location");
+		}
+	}
+
+	/*
+	 * Gets the row number from the user and verifies the entered input 
+	 * is integer value between 1 and 9 only
+	 * Returns the row-1 value as array starts from 0 in java
+	 */
+	private static int acceptRowNumber() {
+		int row;
+		System.out.println("Enter the Row number (Between 1 and 9)");
+		while(true) {
+			while (!reader.hasNextInt()){
+				reader.next();
+				System.out.println("Incorrect Input! Enter valid Row number again (Between 1 and 9)");
+			}
+			row = reader.nextInt();
+			if(row>=1 && row<=9 ) {
+				break;
+			}
+			System.out.println("Incorrect Input! Enter valid Row number again (Between 1 and 9)");
+		}
+		return row-1;
+	}
+	
+	/*
+	 * Gets the column number from the user and verifies the entered input 
+	 * is integer value between 1 and 9 only
+	 * Returns the column-1 value as array starts from 0 in java
+	 */
+	private static int acceptColumnNumber() {
+		int column;
+		System.out.println("Enter the Column number (Between 1 and 9)");
+		while(true) {
+			while (!reader.hasNextInt()){
+				reader.next();
+				System.out.println("Incorrect Input! Enter valid Column number again (Between 1 and 9)");
+			}
+			column = reader.nextInt();
+			if(column>=1 && column<=9 ) {
+				break;
+			}
+			System.out.println("Incorrect Input! Enter valid Column number again (Between 1 and 9)");
+		}
+		return column-1;
+	}
+	
+	/*
+	 * Gets the input value number from the user and verifies the entered input 
+	 * is integer value between 1 and 9 only
+	 * Returns the input value 
+	 */
+	private static int acceptSudokuInputValue() {
+		int value;
+		System.out.println("Enter the Value number (Between 1 and 9)");
+		while(true) {
+			while (!reader.hasNextInt()){
+				reader.next();
+				System.out.println("Incorrect Input! Enter valid Value number again (Between 1 and 9)");
+			}
+			value = reader.nextInt();
+			if(value>=1 && value<=9 ) {
+				break;
+			}
+			System.out.println("Incorrect Input! Enter valid Value number again (Between 1 and 9)");
+		}
+		return value;
+	}
+	
+	/*
+	 * Gets the row number and column number from the user
+	 * Checks if user is allowed to make changes at that location
+	 *  If allowed, removes the value from that location
+	 */
+	private static void removeInputValue(Sudoku obj) {
+		int row = acceptRowNumber();
+		int column = acceptColumnNumber();
+		if(obj.canEditInputValue(row,column)==false) {
+			System.out.println("Cannot edit the number that this location");
+		}
+		else {
+			obj.removeInputValue(row, column);
+			obj.displayInput();
+		}
+	}
+
+	/*
+	 * Checks if all the input values are entered in the sudoku matrix
+	 * If all values are present, checks if the solution is correct or not and
+	 * displays the appropriate message.
+	 */
+	private static void checkBoard(Sudoku obj) {
+		if(obj.checkAllInputValuesPresent()) {
+			if(obj.checkSolution()) {
+				System.out.println("Your solution is correct!!");
+			}
+			else {
+				System.out.println("Check Again!! Your solution is incorrect");
+			}
+		}
+		else {
+			System.out.println("Board not Complete. Enter all the values in the Sudoku Matrix");
+		}
+	}
+	
+	/*
+	 * Checks if all the input values are entered in the sudoku matrix
+	 * Checks if the sudoku has been correctly solved.
+	 * If solved correctly, displays success message and if not
+	 * displays failure message
+	 */
+	private static boolean submitBoard(Sudoku obj) {
+		if(obj.checkAllInputValuesPresent()) {
+			if(obj.checkSolution()) {
+				System.out.println("Congratulations!!!");
+				System.out.println("You have successfully completed the Sudoku!!");
+			}
+			else {
+				System.out.println("Sorry,  Your Solution is incorrect");
+				System.out.println("You may try again from start");
+			}
+			return true;
+		}
+		else {
+			System.out.println("Cannot Submit. Enter all the values in the Sudoku Matrix");
+			return false;
+		}
+	}
+	
+	/*
+	 * Displays the correct solution to the user
+	 */
+	private static void giveupAndDisplaySolution(Sudoku obj) {
+		System.out.println("Thank you for trying. It is first step towards learning");
+		System.out.println("Following is the Correct Solution");
+		obj.displaySolution();
+	}
+	
+	/*
+	 * Re initialized the sudoku board the initial state
+	 */
+	private static void resetBoard(Sudoku obj) {
+		System.out.println("Sudoku Board Reset");
+		obj.initializeBoard();
+		obj.displayInput();
+	}	
+}
---
+++ src/main/java/com/ganesh/SudokuApp/sudoku/Sudoku.java
@@ -0,0 +1,334 @@
+package com.ganesh.SudokuApp.sudoku;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.ganesh.SudokuApp.constants.Constants;
+
+public class Sudoku {
+	
+	// 2D array to maintain input values from the user
+	private int[][] input = new int [Constants.DIMENSION][Constants.DIMENSION];
+	
+	// 2D array to maintain solution values of the sudoku problem
+	private int[][] solution = new int [Constants.DIMENSION][Constants.DIMENSION];
+	
+	/*
+	 * Matrix to differentiate the values between user entered and already present in 
+	 * sudoku problem at the start of the game
+	 */
+	private boolean[][] track = new boolean[Constants.DIMENSION][Constants.DIMENSION];
+	
+	/*
+	 * Hash Map to keep track of the row values for particular row number
+	 * Key is a integer value from 0 to 8 (Mapping Row 1 to Row 9)
+	 * Value is a Set data structure to hold all the values for that particular key
+	 */
+	private Map<Integer, Set<Integer>> rowMap = new HashMap<Integer, Set<Integer>>();
+	
+	// Hash Map to keep track of column values for particular column number
+	private Map<Integer, Set<Integer>> columnMap = new HashMap<Integer, Set<Integer>>();
+	
+	// Hash Map to keep track of the values in the 3*3 sub matrix 
+	private Map<Integer, Set<Integer>> matrixMap = new HashMap<Integer, Set<Integer>>();
+
+	public Sudoku() {
+	}
+	
+	/*
+	 * Used to initialize the Sudoku board
+	 * i.e. initializes Class data members; 
+	 * 2D array and Hash Map 
+	 */
+	public void initializeBoard() {
+		
+		initializeMatrix();
+		
+		initializeMapAndSet();
+	}
+
+	/*
+	 * Initialize the 2D array data members
+	 * Initial values are obtained from the Constants class
+	 * track is a boolean array; value is set to true if input at 
+	 * that location is non zero else set to false 
+	 */
+	private void initializeMatrix() {
+		for(int i=0; i<input.length; i++) {
+			for(int j=0; j<input[0].length; j++) {
+				input[i][j] = Constants.SAVEDINPUT[i][j];
+				solution[i][j] = Constants.SAVEDSOLUTION[i][j];
+				if(Constants.SAVEDINPUT[i][j] == 0) {
+					track[i][j] = false;
+				}
+				else {
+					track[i][j] = true;
+				}
+			}
+		}
+	}
+	
+	/*
+	 * Initialize the Hash Map. 
+	 * Initialized the Set for each key of the hash map
+	 * Add values present in the input matrix in the set
+	 */
+	private void initializeMapAndSet() {
+		for(int i=0; i<Constants.DIMENSION; i++) {
+			 rowMap.put(i, new HashSet<Integer>());
+			 columnMap.put(i, new HashSet<Integer>());
+			 matrixMap.put(i, new HashSet<Integer>());
+		}
+		
+		for(int i=0; i<Constants.DIMENSION; i++) {
+			for(int j=0; j<Constants.DIMENSION; j++) {
+				if(track[i][j]==true) {
+					addValueInMapAndSet(i, j, input[i][j]);
+				}
+			}
+		}
+	}
+
+	
+	/*
+	 * Method accepts parameter as row, column and value.
+	 * Get the set for that row number, add row value in the set and 
+	 * add the set for at that row value key in the Hash map
+	 * Same is done for Column map and Matrix map
+	 */
+	private void addValueInMapAndSet(int row, int column, int value) {
+		Set<Integer> set;
+		set = rowMap.get(row);
+		set.add(value);
+		rowMap.put(row, set);
+		
+		set = columnMap.get(column);
+		set.add(value);
+		columnMap.put(column, set);
+		
+		int k = getLookupIndex(row, column);
+		set = matrixMap.get(k);
+		set.add(value);
+		matrixMap.put(k, set);
+	}
+
+	/*
+	 * Displays the input matrix
+	 * It displays blank space if the value in input array is zero
+	 * else it display the value
+	 */
+	public void displayInput() {
+		
+		final String ANSI_RED = "\u001B[31m";
+		final String ANSI_RESET = "\u001B[0m";
+		
+		System.out.println();
+		for(int i=0;i<input.length;i++) {
+			for(int j=0; j<input[0].length;j++) {
+				if(j%3==0 && j!=0) {
+					System.out.print("| ");
+				}
+				if(input[i][j] != 0 && track[i][j]==true) {
+					System.out.print(input[i][j]+" ");
+				}
+				else if(input[i][j]!=0 && track[i][j]==false){
+					System.out.print(input[i][j]+" ");
+					//System.out.println(ANSI_RED+input[i][j]+ANSI_RESET);
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			if((i+1)%3==0) {
+				System.out.println();
+				System.out.println("-----------------------");
+			}
+			else {
+				System.out.println();
+			}
+		}
+	}
+	
+	/*
+	 * Method accepts the row, column and value as parameter
+	 * This method verifies if the entered value is valid value 
+	 * at that location in sudoku
+	 * It return true if value is valid else return false
+	 */
+	public boolean verifyInput(int row, int column, int value) {
+		Set<Integer> set ;
+		set = rowMap.get(row);
+		if(set.contains(value)) {
+			return false;
+		}
+		
+		set = columnMap.get(column);
+		if(set.contains(value)) {
+			return false;
+		}
+		
+		set = matrixMap.get(getLookupIndex(row, column));
+		if(set.contains(value)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/*
+	 * Method accepts parameter as row, column and value.
+	 * Value is added to row and column in input array and 
+	 * in the Hash Maps
+	 */
+	public void enterInput(int row, int column, int value) {
+		
+		input[row][column] = value;
+		addValueInMapAndSet(row, column, value);
+		
+	}
+	
+	/*
+	 * This method accepts the row and column number as parameters
+	 * This methods maps the row and column number to the matrix map key
+	 * Return the key value to the corresponding row and column number.
+	 */
+	private int getLookupIndex(int row, int column) {
+		if(row>=0 && row<=2 && column>=0 && column<=2) {
+			return 0;
+		}
+		else if(row>=0 && row<=2 && column>=3 && column<=5) {
+			return 1;
+		}
+		else if(row>=0 && row<=2 && column>=6 && column<=8) {
+			return 2;
+		}
+		else if(row>=3 && row<=5 && column>=0 && column<=2) {
+			return 3;
+		}
+		else if(row>=3 && row<=5 && column>=3 && column<=5) {
+			return 4;
+		}
+		else if(row>=3 && row<=5 && column>=6 && column<=8) {
+			return 5;
+		}
+		else if(row>=6 && row<=8 && column>=0 && column<=2) {
+			return 6;
+		}
+		else if(row>=6 && row<=8 && column>=3 && column<=5) {
+			return 7;
+		}
+		else {
+			return 8;
+		}
+	}
+
+	/*
+	 * Method accepts row and column number as parameters
+	 * This method verifies if user can make changes at that 
+	 * location of row and column
+	 * Return true if changes are allowed else return false
+	 */
+	public boolean canEditInputValue(int row, int column) {
+		if(track[row][column]==true) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * Method accepts row and column number as parameters
+	 * Return true if input value is non zero at that location 
+	 * else return false
+	 */
+	public boolean canChangeInputValue(int row, int column) {
+		if(input[row][column]!=0) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	/*
+	 * Method accepts row and column number as parameters
+	 * Set the input array value to zero at that location
+	 * Removes the value from all the Hash Maps
+	 */
+	public void removeInputValue(int row, int column) {
+		
+		int replace = input[row][column];
+		input[row][column]=0;
+		
+		Set<Integer> set;
+		set = rowMap.get(row);
+		set.remove(replace);
+		rowMap.put(row, set);
+		
+		set = columnMap.get(column);
+		set.remove(replace);
+		columnMap.put(column, set);
+		
+		int index = getLookupIndex(row, column);
+		set = matrixMap.get(index);
+		set.remove(replace);
+		matrixMap.put(index, set);
+	}
+
+	/*
+	 * Check if all the values in the input matrix are entered 
+	 * by the user.
+	 * Return true if all values are present else return false
+	 */
+	public boolean checkAllInputValuesPresent(){
+		
+		for(int i=0;i<Constants.DIMENSION; i++) {
+			if((rowMap.get(i)).size() !=9) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+	/*
+	 * Check the input matrix values with the solution matrix
+	 * Return false if it is not equal else return true
+	 */
+	public boolean checkSolution() {
+		
+		for(int i=0; i<Constants.DIMENSION; i++) {
+			for(int j=0; j<Constants.DIMENSION; j++) {
+				if(input[i][j] != solution[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+		
+	}
+
+	/*
+	 * Display the solution matrix values
+	 */
+	public void displaySolution() {
+		
+		System.out.println();
+		for(int i=0; i<Constants.DIMENSION; i++) {
+			for(int j=0; j<Constants.DIMENSION; j++) {
+				
+				if(j%3==0 && j!=0) {
+					System.out.print("| ");
+				}
+				System.out.print(solution[i][j]+" ");
+			}
+			if((i+1)%3==0) {
+				System.out.println();
+				System.out.println("-----------------------");
+			}
+			else {
+				System.out.println();
+			}
+		}	
+	}
+}
